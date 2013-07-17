25b8c1de00ae12c2153bda8be2a178195d1fdc71
Thomas Cuje <tcuje@freenet.de>
2013-07-17 15:16:14 +0200

    formatting

--- WPS/src/de/rwth/ti/common/WPSQuadKnot.java
+++ WPS/src/de/rwth/ti/common/WPSQuadKnot.java
@@ -19,29 +19,33 @@ public class WPSQuadKnot {
 		br = null;
 	}
 
-	public MeasurePoint getValue(){
+	public MeasurePoint getValue() {
 		return value;
 	}
-	
-	public void setValue(MeasurePoint mp){
+
+	public void setValue(MeasurePoint mp) {
 		value = mp;
 	}
-	
+
 	public void addMPoint(MeasurePoint mp) {
 		if (tl == null && value == null) {
 			value = mp;
-		}else{
-			if(tl==null){
-				tl = new WPSQuadKnot(xMin, (xMax+xMin)/2, yMin, (yMin+yMax)/2);
-				tr = new WPSQuadKnot((xMax+xMin)/2, xMax, yMin, (yMin+yMax)/2);
-				bl = new WPSQuadKnot(xMin, (xMax+xMin)/2, (yMin+yMax)/2, yMax);
-				br = new WPSQuadKnot((xMax+xMin)/2, xMax, (yMin+yMax)/2, yMax);
+		} else {
+			if (tl == null) {
+				tl = new WPSQuadKnot(xMin, (xMax + xMin) / 2, yMin,
+						(yMin + yMax) / 2);
+				tr = new WPSQuadKnot((xMax + xMin) / 2, xMax, yMin,
+						(yMin + yMax) / 2);
+				bl = new WPSQuadKnot(xMin, (xMax + xMin) / 2,
+						(yMin + yMax) / 2, yMax);
+				br = new WPSQuadKnot((xMax + xMin) / 2, xMax,
+						(yMin + yMax) / 2, yMax);
 			}
-			if(value != null){
-				if(value.getPosx()>(xMax+xMin)/2){
-					if(value.getPosy()>(yMax+yMin)/2){
+			if (value != null) {
+				if (value.getPosx() > (xMax + xMin) / 2) {
+					if (value.getPosy() > (yMax + yMin) / 2) {
 						br.addMPoint(value);
-					}else{
+					} else {
 						tr.addMPoint(value);
 					}
 				} else {
@@ -68,16 +72,16 @@ public class WPSQuadKnot {
 			}
 		}
 	}
-	
-	public WPSQuadKnot getKnot(float x,float y){
-		if(x>((xMax+xMin)/2)){
-			if(y>((yMax+yMin)/2)){
+
+	public WPSQuadKnot getKnot(float x, float y) {
+		if (x > ((xMax + xMin) / 2)) {
+			if (y > ((yMax + yMin) / 2)) {
 				return br;
 			} else {
 				return tr;
 			}
-		}else{
-			if(y > ((yMax+yMin)/2)){
+		} else {
+			if (y > ((yMax + yMin) / 2)) {
 				return bl;
 			} else {
 				return tl;
